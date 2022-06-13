95be1be3b6f41c9f5206f155d90951a1703e0a21
quwensong <1607186824@qq.com>
2022-06-13 12:18:15 +0800

    🐞 fix:
    demo-fix修改

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import "@/assets/css/reset.css"
 import App from './App'
-
+console.log(ReactDOM)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
